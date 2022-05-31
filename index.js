88a969b22ea265bf88542f60b70377a15a702f62
AnhydraseTech <101112779+AnhydraseTech@users.noreply.github.com>
2022-05-31 13:10:03 +0100

    notes

---
+++ index.js
@@ -0,0 +1,16 @@
+ //Keypoints from first react class
+
+ //Reactjs is for WebApp while React Native is for Mobile App 
+ //React builds App by combining differening components
+ // The codes in the return()statement is rendered on the browser screen
+ // You need to install nodejs before you can use reactjs
+ // To install Nodejs, go to https://nodejs.org/en/download/ and select 
+ //the one suitable for your OS and machine
+
+
+
+ // How to install create-react-app with CLI
+ // Open your CLI and navigate to the folder where you wish to create the app
+ // Enter this:npx create-react-app my-app cd my-app npm start
+ // i.e create-react-app (name of your app)  cd (your app) npm start
+
